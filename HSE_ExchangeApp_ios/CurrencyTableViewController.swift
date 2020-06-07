2ba67ef99d0bff3bb13e658671053837a9c568f1
yoloroy <slart2108@gmail.com>
2020-06-07 23:32:48 +0300

    add api sync

--- HSE_ExchangeApp_ios/CurrencyTableViewController.swift
+++ HSE_ExchangeApp_ios/CurrencyTableViewController.swift
@@ -4,19 +4,19 @@ class CurrencyTableViewController: UITableViewController {
     var items = [Currency]()
     var convertValues: [String: Double] = [:]
     
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         loadDefaults()
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return items.count
     }
@@ -25,7 +25,6 @@ class CurrencyTableViewController: UITableViewController {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "CurrencyTableViewCell", for: indexPath) as? CurrencyTableViewCell else {
             fatalError("The dequeued cell is not an instance of CurrencyTableViewCell.")
         }
-        print(indexPath.row)
         
         cell.tag = indexPath.row
         cell.setData(newData: items[indexPath.row])
@@ -34,8 +33,21 @@ class CurrencyTableViewController: UITableViewController {
     }
     
     private func loadDefaults() {
-        convertValues["USD"] = 1
-        convertValues["RUB"] = 74
+        let url = URL(string: "https://api.exchangeratesapi.io/latest")!
+        
+        let task = URLSession.shared.dataTask(with: url) {(data, response, error) in
+            DispatchQueue.main.async {
+                do {
+                    if let json = try JSONSerialization.jsonObject(with: data!, options: []) as? [String: Any] {
+                        self.convertValues = (json["rates"] as! [String: Double])
+                        self.convertValues["EUR"] = 1.0
+                    }
+                } catch let error as NSError {
+                    print("Failed to load: \(error.localizedDescription)")
+                }
+            }
+        }
+        task.resume()
         
         items += [Currency(kind: "USD", value: 0.0)]
         items += [Currency(kind: "RUB", value: 0.0)]
@@ -44,10 +56,10 @@ class CurrencyTableViewController: UITableViewController {
     @IBAction func chooseCurrency(_ sender: UIButton) {
         // TODO: add choose
         let alert = UIAlertController(title: "Alert title", message: "Alert message.", preferredStyle: .alert)
-
+        
         alert.addAction(UIAlertAction(title: "Yes", style: .default, handler: nil))
         alert.addAction(UIAlertAction(title: "No", style: .cancel, handler: nil))
-
+        
         self.present(alert, animated: true)
     }
     
@@ -72,5 +84,13 @@ class CurrencyTableViewController: UITableViewController {
 extension String {
     func toDouble() -> Double{
         return (self as NSString).doubleValue
+    }
+}
+extension Dictionary {
+    static func + <K, V> (left: inout [K:V], right: [K:V]) -> [K:V] {
+        for (k, v) in right {
+            left[k] = v
+        }
+        return left
     }
 }
